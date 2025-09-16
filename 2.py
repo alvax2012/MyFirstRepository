5b9ebb56c8e17b0a067e2ead1a8aab5a7e36175d
alvax2012 <39165799+alvax2012@users.noreply.github.com>
2025-09-16 18:12:58 +0800

    Update 2.py

--- 2.py
+++ 2.py
@@ -1,4 +1,5 @@
 ﻿import random
+import string
 
 n = 7    # количество попыток
 s = {0: 'Орел', 1: 'Решка'}
@@ -21,3 +22,41 @@ while len(s) < 7:
     s.add(random.randrange(1, 50))
 s = list(s)
 print(*sorted(s))
+
+
+def generate_index():
+    s = string.ascii_uppercase
+    return f'{random.choice(s)}{random.choice(s)}{random.randrange(100)}_{random.randrange(100)}{random.choice(s)}{random.choice(s)}'
+
+
+print(generate_index())
+
+print('--'.join('asd'))
+
+matrix = [[1, 2, 3, 4],
+          [5, 6, 7, 8],
+          [9, 10, 11, 12],
+          [13, 14, 15, 16]]
+random.shuffle(matrix)
+print(matrix)
+
+n = string.digits
+s = [[random.choice(n) if _ != 0 else random.randrange(1, 10)
+      for _ in range(7)] for _ in range(3)]
+
+for _ in s:
+    print(*_, sep='')
+
+
+s = [int(_) for _ in random.sample(string.digits, 6)]
+
+s.insert(0, random.randrange(1, 10))
+# print(s)
+s = [random.sample(range(10), 7) for _ in range(3)]
+print(s)
+
+s = 'липа'
+# s = list(s)
+s = random.sample(s, len(s))
+# random.shuffle(s)
+print(''.join(s))
