154418cf9487acb5fc3355aef4b392f0e0a5e2b6
alvax2012 <39165799+alvax2012@users.noreply.github.com>
2025-10-10 17:58:16 +0800

    Update 9.py

--- 9.py
+++ 9.py
@@ -49,3 +49,17 @@ def info_kwargs(**kwargs):
 
 
 info_kwargs(first_name='Timur', last_name='Guev', age=28, job='teacher')
+
+print(type(sum))
+print(sum)
+
+numbers = [(10, 10, 10), (30, 45, 56), (81, 39), (1, 2, 3), (12,), (-2, -4, 100), (1, 2, 99), (89, 9, 34), (10,
+                                                                                                            20, 30, -2), (50, 40, 50), (34, 78, 65), (-5, 90, -1, -5), (1, 2, 3, 4, 5, 6), (-9, 8, 4), (90, 1, -45, -21)]
+
+
+def sr(num):
+    return sum(num) / len(num)
+
+
+# print(sr(numbers[0]))
+print(min(numbers, key=sr), max(numbers, key=sr), sep='\n')
