8d9335fc1f88f78ae28926deb90fed157567f36c
alvax2012 <39165799+alvax2012@users.noreply.github.com>
2025-10-15 10:22:34 +0800

    Update 9.py

--- 9.py
+++ 9.py
@@ -102,10 +102,6 @@ l = 'a'
 print(l[::-1])
 
 
-def rnd():
-    return round(x, 2)
-
-
 def map(function, items):
     result = []
     for item in items:
@@ -115,3 +111,6 @@ def map(function, items):
 
 numbers = [3.56773, 5.57668, 4.00914, 56.24241, 9.01344, 32.12013,
            23.22222, 90.09873, 45.45, 314.1528, 2.71828, 1.41546]
+
+
+print()
