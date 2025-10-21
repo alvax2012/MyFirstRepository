1a4764bc6de5167b909bdb1f56aaa8c739c2ac1e
alvax2012 <39165799+alvax2012@users.noreply.github.com>
2025-10-22 07:24:29 +0800

    Update 14.py

--- 14.py
+++ 14.py
@@ -45,3 +45,16 @@ l = list(sorted(map(lambda x: x[0], filter(
 
 
 print(reduce(lambda x, y: f'{x} {y},', l, 'Cities:')[:-1])
+
+
+def is_non_negative_num(x): return [_ for _ in x if _ in '.']
+
+
+print(is_non_negative_num('10.34ab'))
+print(is_non_negative_num('10.45'))
+print(is_non_negative_num('-18'))
+print(is_non_negative_num('-34.67'))
+print(is_non_negative_num('987'))
+print(is_non_negative_num('abcd'))
+print(is_non_negative_num('123.122.12'))
+print(is_non_negative_num('123.122'))
