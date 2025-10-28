ec6bebfed2bbb79f12df103e13fafae78ee0faad
alvax2012 <39165799+alvax2012@users.noreply.github.com>
2025-10-28 18:37:31 +0800

    1

--- 17.py
+++ 17.py
@@ -5,15 +5,15 @@ class Person:
         self.dt = dt1
         self.cnt = int(cnt1)
 
-    def display_info(self):
+    def qar(self):
         l = int(self.dt.split('-')[1])
         q = 0
         if l == 6:
             q = 7
         elif l == 3:
-            q == 4
+            q = 4
         else:
-            q == 3
+            q = 0
         return q
 
 
@@ -30,15 +30,65 @@ def report1(s):
         # print(i)
         p = Person(i[0], i[1], i[2])
         m.append(
-            f'Q: {p.display_info()}\n Наименование: {p.name} \n Количество: {p.cnt}')
-        d[i[0]] = d.setdefault(i[0], 0) + 1
-    print(d)
-    return m
+            f'Q: {p.qar()}\n Наименование: {p.name} \n Количество: {p.cnt}')
+        d[p.qar()][p.name] = d.setdefault(
+            p.qar(), {}).setdefault(p.name, 0) + p.cnt
+    # print('d', l, m, d, sep='\n')
+    return d
 
 
-s = '2025-06-01:ручка:3;2025-06-01:стол:3;2025-03-02:стол:5'
+s = '2025-06-01:ручка:3;2025-06-01:стол:3;2025-03-01:ручка:3;2025-03-02:стол:5;2025-01-01:ручка:3;2025-02-02:ручка:6'
 tt = report1(s)
-print(report1(s))
+print(tt)
 
-for i in tt:
-    print(i)
+for k, v in tt.items():
+    print(f'Q: {k}\n Наименование: {v.items()} \n Количество: {v.values()}')
+
+
+numbers = [9, 8, 32, 1, 10, 1, 10, 23, 1, 4, 10,
+           4, 2, 2, 2, 2, 1, 10, 1, 2, 2, 32, 23, 23]
+
+result = {}
+for num in numbers:
+    # result[num] = result.get(num, 0) + 1
+    result.setdefault(num, []).append(True)
+    result1 = map(lambda x: (x[0], len(x[1])), result.items())
+# print('res1', *result1)
+
+
+s1 = 'ZIPZIZ'
+p = {1: 'AEIOULNSTR',
+        2: 'DGZ',
+        3: 'BCMP',
+        4: 'FHVWY',
+        5: 'K',
+        8: 'JX',
+        10: 'Q'}
+t1 = [k for i in s1 for k, v in p.items() if i in v]
+t2 = [k for k, v in p.items() for _ in s1 if _ in v]
+# print(sum(t1), sum(t2), sep='\n')
+
+
+def merge(l):
+    res = {}
+    for d in l:
+        for k, v in d.items():
+
+            res.setdefault(k, set()).add(v)
+            # print(type(res.setdefault(k, set())))
+    return res
+
+
+result = merge([{'a': 1, 'b': 2}, {'b': 10, 'c': 100}, {
+               'a': 1, 'b': 17, 'c': 50}, {'a': 5, 'd': 777}])
+# print(result)
+
+
+dict1 = {'яблоки': 100, 'бананы': 333, 'груши': 200,
+         'апельсины': 300, 'ананасы': 45, 'лимоны': 98,
+         'сливы': 76, 'манго': 34, 'виноград': 90, 'лаймы': 230}
+dict2 = {'яблоки': 300, 'груши': 200, 'бананы': 400,
+         'малина': 777, 'ананасы': 12, 'лаймы': 123, 'черника': 111, 'арбузы': 666}
+merged_dict = {key: dict1.get(key, 0) + dict2.get(key, 0)
+               for key in set(dict1) | set(dict2)}
+# print("Объединенный словарь:", merged_dict)
--- 22.py
+++ 22.py
@@ -56,3 +56,12 @@ except Exception as e:
 print(videos)
 
 print('--', vd1)
+
+
+numbers = [9, 8, 32, 1, 10, 1, 10, 23, 1, 4, 10,
+           4, 2, 2, 2, 2, 1, 10, 1, 2, 2, 32, 23, 23]
+
+result = {}
+for num in numbers:
+    result[num] = result.get(num, 0) + 1
+print(result)
