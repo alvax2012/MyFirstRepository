fcbae3ce6555ab7f25994d115716113bbbfff074
alvax2012 <39165799+alvax2012@users.noreply.github.com>
2025-11-01 20:52:42 +0800

    Update 19.py

--- 19.py
+++ 19.py
@@ -1,9 +1,9 @@
 from operator import mul
 from functools import reduce
 p1 = 'c:/1/'
-print(p1 + 'lines2.txt')
+print(p1 + 'lines.txt')
 
-with open(p1 + 'lines2.txt', encoding='utf-8') as f:
+with open(p1 + 'lines.txt', encoding='utf-8') as f:
     l1 = f.readlines()
     m = len(max(l1, key=lambda x: len(x)))
     # print(m)
@@ -16,17 +16,23 @@ with open(p1 + 'lines2.txt', encoding='utf-8') as f:
     # print(l2)
     print(reduce(lambda x, y: x + y if len(y) == m else x, l1))
 
-with open(p1 + 'nums3.txt', encoding='utf-8') as f:
-    # l1 = f.readlines()
-    # m = len(max(l1, key=lambda x: len(x)))
-    # print(m)
-    # f.seek(0)
+print()
+with open(p1 + 'nums.txt', encoding='utf-8') as f:
+    print([list(map(lambda x: x, i)) for i in f])
+# l1 = f.readlines()
+# m = len(max(l1, key=lambda x: len(x)))
+# print(m)
+# f.seek(0)
 
-    print(*map(lambda x: sum(list(map(int, x.strip().split()))), f.readlines()), sep='\n')
+# print(*map(lambda x: sum(list(map(int, x.strip().split()))), f.readlines()), sep='\n')
 
-    # print(*map(lambda x: sum(list(map(int, x.strip().split()))), f.readlines()), sep='\n')
-    # print(*filter(lambda x: len(x) >= 19, l1))
-    # l2 = ['1', '33', '444']
-    # l2 = [_.replace(' ', '_').strip() for _ in f.readlines() if len(_) > 17]
-    # print(l2)
-    # print(reduce(lambda x, y: x + y if len(y) == m else x, l1))
+# print(*map(lambda x: sum(list(map(int, x.strip().split()))), f.readlines()), sep='\n')
+# print(*filter(lambda x: len(x) >= 19, l1))
+# l2 = ['1', '33', '444']
+# l2 = [_.replace(' ', '_').strip() for _ in f.readlines() if len(_) > 17]
+# print(l2)
+# print(reduce(lambda x, y: x + y if len(y) == m else x, l1))
+print()
+
+l = ' 12   45 456   3 \n'
+print(l.split())
