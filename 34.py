7fb062c0af7db493227a94866c50bf4b6af9b278
alvax2012 <39165799+alvax2012@users.noreply.github.com>
2025-11-24 00:43:10 +0800

    Update 34.py

--- 34.py
+++ 34.py
@@ -37,3 +37,23 @@ print('Цифра в позиции тысяч равна', d1, num // 1000)
 print('Цифра в позиции сотен равна', d2, (num // 100) % 10, num // 100)
 print('Цифра в позиции десятков равна', d3, (num // 10) % 10, num // 10)
 print('Цифра в позиции единиц равна', d4, num % 10)
+
+
+def get_biggest(l: list):
+    if l == []:
+        return -1
+    ls = list(map(str, l))
+    n = len(max(ls, key=lambda x: len(x)))  # len(''.join(ls))
+    return int(''.join(sorted(ls, key=lambda x: x*n, reverse=True))), sorted(ls, key=lambda x: x.ljust(n, x[0]), reverse=True)
+    # ls1 = sorted(ls, key=lambda x: tuple(x),  reverse=True)
+    # return ls1
+
+
+print(get_biggest([61, 228, 9, 3, 11]))
+print(get_biggest([7, 71, 72]))
+print(get_biggest([13, 221, 423, 53, 1, 2, 33, 58, 78554, 34,
+      65, 65, 2, 1]), 78554656558534233433222211311, sep='\n')
+
+print(get_biggest([13, 1, 1]))
+
+print(get_biggest([62, 626]))
