34767153143ec676f4130d2437037688d15ab345
alvax2012 <39165799+alvax2012@users.noreply.github.com>
2025-11-28 07:55:51 +0800

    1

--- 35.py
+++ 35.py
@@ -5,4 +5,15 @@ ru = 'АаВСсЕеНКМОоРрТХху'
 
 s1, s2, s3 = 'Р', 'О', 'А'
 l = (s1, s2, s3)
-print(any(map(lambda x: x in en, l)))
+if all(map(lambda x: x in en, l)):
+    print('en')
+elif all(map(lambda x: x in ru, l)):
+    print('ru')
+else:
+    print('mix')
+
+langs = ['ru', 'mix', 'mix', 'en']
+eng = 'AaBCcEeHKMOoPpTXxy'
+ind = sum(_ in eng for _ in l)
+print(*(_ in eng for _ in l))
+print(langs[ind])
