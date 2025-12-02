19a1e85548831fd58b8b4fecf035f532bf81c194
alvax2012 <39165799+alvax2012@users.noreply.github.com>
2025-12-03 07:44:08 +0800

    Update 39.py

--- 39.py
+++ 39.py
@@ -21,11 +21,43 @@ print(get_min_max(dates))
 
 
 def get_date_range(start, end):
-    return date.toordinal(end - start)  # [i for i in range(end - start)]
+    return [date.fromordinal(i) for i in range(date.toordinal(start), date.toordinal(end)+1)]
 
 
-date1 = date(2021, 10, 1)
-date2 = date(2021, 10, 5)
+date1 = date(2025, 12, 25)
+date2 = date(2026, 1, 5)
 
-print(date2 - date1)
-print(get_date_range(date1, date2), sep='\n')
+# print(date2 - date1)
+print(get_date_range(date1, date2))
+
+
+def saturdays_between_two_dates(start, end):
+    if start > end:
+        start, end = end, start
+    # return len([date.fromordinal(i) for i in range(date.toordinal(start), date.toordinal(end)+1) if date.fromordinal(i).weekday() == 6])
+    return sum(date.fromordinal(i).weekday() == 5 for i in range(start.toordinal(), end.toordinal() + 1))
+
+
+date1 = date(2010, 6, 13)
+date2 = date(2011, 7, 14)
+print(saturdays_between_two_dates(date1, date2))
+
+
+def saturdays_between_two_dates(date1, date2):
+    return (max(date1, date2).toordinal() - min(date1, date2).toordinal() - (8 - min(
+        date1, date2).isoweekday()) - max(date1, date2).isoweekday() + 1) // 7 + 1 - min(date1, date2).isoweekday() // 7 + max(date1, date2).isoweekday() // 6
+
+
+date2 = date(2023, 11, 5)
+date1 = date(2023, 11, 24)
+
+
+def saturdays_between_two_dates(start, end):
+    if start > end:
+        start, end = end, start
+    delta = end.toordinal() - start.toordinal()
+    print(delta//7, start.weekday(), delta % 7)
+    return delta//7 + (start.weekday() % 6+delta % 7 >= 5)
+
+
+print(saturdays_between_two_dates(date1, date2))
