9ade760e2599fadd532789569bcf86a2b0771968
alvax2012 <39165799+alvax2012@users.noreply.github.com>
2025-12-09 18:22:43 +0800

    Update 41.py

--- 41.py
+++ 41.py
@@ -20,29 +20,25 @@ print('111')
 
 
 def is_available_date(booked_dates, date_for_booking):
-    bd = []
-    b_d = {}
+    d = {}
     fmt = '%d.%m.%Y'
     for i in booked_dates:
         if '-' not in i:
-            bd.append([i])
-            b_d.setdefault(datetime.strptime(i, fmt), 1)
+            d.setdefault(datetime.strptime(i, fmt), datetime.strptime(i, fmt))
         else:
-            bd.append(i.split('-'))
-            b_d.setdefault(datetime.strptime(i.split(
-                '-')[0], fmt), (datetime.strptime(i.split('-')[1], fmt) - datetime.strptime(i.split('-')[0], fmt)).days+1)
+            d.setdefault(datetime.strptime(
+                i.split('-')[0], fmt), datetime.strptime(i.split('-')[1], fmt))
 
-    res = 0
-    date_for_booking_dt = datetime.strptime(date_for_booking, fmt)
-    date_for_booking_cnt = 1
-    for k, v in sorted(b_d.items(), key=lambda x: x[0]):
-        print(k, v)
-        if date_for_booking_dt >= k and date_for_booking_cnt <= v:
-            res = date_for_booking, k, v
-            break
-    return res
+    if '-' in date_for_booking:
+        d1 = datetime.strptime(date_for_booking.split('-')[0], fmt)
+        d2 = datetime.strptime(date_for_booking.split('-')[1], fmt)
+    else:
+        d1 = datetime.strptime(date_for_booking, fmt)
+        d2 = d1
+    print(d, d1, d2)
+    return not any([(d1 >= k and d1 <= v) or (d2 >= k and d2 <= v) for k, v in d.items()])
 
 
 dates = ['04.11.2021', '05.11.2021-09.11.2021']
-some_date = '05.11.2021'
+some_date = '01.11.2021-04.11.2021'
 print(is_available_date(dates, some_date))
