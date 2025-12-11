9eb7fd4b200c3ae3f54042f377d4b54daa69acf0
alvax2012 <39165799+alvax2012@users.noreply.github.com>
2025-12-11 18:22:11 +0800

    Update 41.py

--- 41.py
+++ 41.py
@@ -1,4 +1,6 @@
-from datetime import datetime, date
+from datetime import datetime, date, timedelta
+from math import ceil
+
 fmt = '%d.%m.%Y; %H:%M'
 d = {}
 with open('diary.txt', encoding='utf-8') as f:
@@ -11,9 +13,9 @@ with open('diary.txt', encoding='utf-8') as f:
 l = sorted(d.items(), key=lambda x: x[0])
 d[k] += ['']
 
-for k in sorted(d):
-    print(k.strftime(fmt))
-    print(*d[k], sep='\n')
+# for k in sorted(d):
+#     print(k.strftime(fmt))
+#     print(*d[k], sep='\n')
 
 
 print('111')
@@ -36,9 +38,46 @@ def is_available_date(booked_dates, date_for_booking):
         d1 = datetime.strptime(date_for_booking, fmt)
         d2 = d1
     print(d, d1, d2)
-    return not any([(d1 >= k and d1 <= v) or (d2 >= k and d2 <= v) for k, v in d.items()])
+    return not any([any(((d1 >= k and d1 <= v), (d2 >= k and d2 <= v), (k >= d1 and k <= d2), (v >= d1 and v <= d2))) for k, v in d.items()])
 
 
-dates = ['04.11.2021', '05.11.2021-09.11.2021']
-some_date = '01.11.2021-04.11.2021'
+dates = ['01.11.2021', '05.11.2021-09.11.2021',
+         '12.11.2021', '15.11.2021-21.11.2021']
+some_date = '10.11.2021-14.11.2021'
 print(is_available_date(dates, some_date))
+
+
+ans = datetime.now() + timedelta(days=ceil(timedelta(hours=93) / timedelta(hours=5)))
+print(ans.strftime('Приблизительная дата окончания курса: %d.%m.%Y %H:%M'))
+
+
+dt = datetime(2021, 11, 4, 13, 6) + timedelta(weeks=1, hours=12)
+
+print(dt.strftime('%d.%m.%Y %H:%M:%S'))
+
+
+today = date(2021, 11, 4)
+birthday = date(2022, 10, 6)
+
+days = abs(today-birthday).days
+
+print(days)
+
+
+d1 = datetime.strptime('04.11.2021', '%d.%m.%Y')
+d0 = datetime.strftime(d1+timedelta(days=-1), '%d.%m.%Y')
+d2 = datetime.strftime(d1+timedelta(days=1), '%d.%m.%Y')
+print(d0, d2, sep='\n')
+
+
+ts = '%H:%M:%S'
+d1 = datetime.strptime('00:01:01', ts)
+d0 = datetime.strptime('00:00:00', ts)
+print((d1-d0).seconds)
+
+
+ts = '%H:%M:%S'
+d1 = datetime.strptime('09:00:00', ts)
+n = 90  # int(input())
+
+print(datetime.strftime(d1 + timedelta(seconds=n), ts))
