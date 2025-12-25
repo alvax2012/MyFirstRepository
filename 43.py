56159fb376ba59e6cec65598cc3b5a0284c4985f
alvax2012 <39165799+alvax2012@users.noreply.github.com>
2025-12-25 15:00:32 +0800

    Update 43.py

--- 43.py
+++ 43.py
@@ -1,14 +1,60 @@
 
 from datetime import datetime, date, time, timedelta
 
-t0 = '%d.%m.%Y'
-t1 = '%d.%m'
-d0 = '14.11.2021'
-d0 = datetime.strptime(d0, t0)
-l = [['Иван Петров', '16.11.1995'], ['Петр Сергеев',
-                                     '14.11.1997'], ['Сергей Романов', '17.11.1994']]
-d0_cnt = (d0 - datetime(year=d0.year, month=1, day=1)).days
-dl = {datetime.strptime(k, t0): v for v, k in l if (datetime.strptime(
-    k, t1) - datetime.strptime(year=datetime.strptime(k, t1).year, month=1, day=1)) > 100}
+# t0 = '%d.%m.%Y'
+# t1 = '%d.%m'
+# d0 = '14.11.2021'
+# d0 = datetime.strptime(d0, t0)
+# l = [['Иван Петров', '16.11.1995'], ['Петр Сергеев',
+#                                      '14.11.1997'], ['Сергей Романов', '17.11.1994']]
+# d0_cnt = (d0 - datetime(year=d0.year, month=1, day=1)).days
+# dl = {datetime.strptime(k, t0): v for v, k in l if (datetime.strptime(
+#     k, t1) - datetime.strptime(year=datetime.strptime(k, t1).year, month=1, day=1)) > 100}
 
-print(d0_cnt)
+# print(d0_cnt)
+
+
+def choose_plural(amount: int, declensions: tuple):
+    p = ((amount // 10) % 10)*10 + (amount % 10)
+    if p % 10 == 0:
+        i = 2
+    elif amount % 10 in range(5, 10) or p in range(11, 20):
+        i = 2
+    elif amount % 10 == 1:
+        i = 0
+    elif amount % 10 in range(2, 5):
+        i = 1
+    return f'{amount} {declensions[i]}'
+
+
+t0 = '%d.%m.%Y %H:%M'
+# d0 = datetime.strptime('16.11.2021 06:30', t0)
+# d1 = datetime.strptime('08.11.2022 12:00', t0)
+
+d0 = datetime.strptime('16.11.2021 12:30', t0)
+d1 = datetime.strptime('17.11.2021 14:10', t0)
+
+d_cnt = (d1 - d0).days
+
+
+s_all = (d1 - d0).seconds
+h_cnt = s_all // 3600
+m_cnt = (s_all // 60) % 60
+s_cnt = s_all % 60
+
+if d_cnt < 0:
+    print(f'Курс уже вышел!')
+if d_cnt != 0 and h_cnt != 0 and m_cnt != 0:
+    print(f'До выхода курса осталось: {d_cnt} дней и {h_cnt} часов')
+elif h_cnt == 0:
+    print(f'До выхода курса осталось: {d_cnt} дней')
+elif d_cnt == 0:
+    print(f'До выхода курса осталось: {h_cnt} часов и {m_cnt} минут')
+elif m_cnt == 0:
+    print(f'До выхода курса осталось: {h_cnt} час')
+elif h_cnt == 0:
+    print(f'До выхода курса осталось: {m_cnt} мин')
+
+
+print('d_cnt=', d_cnt, 'h_cnt=', h_cnt, 'm_cnt=',
+      m_cnt, 's_cnt=', s_cnt, 's_all', s_all)
