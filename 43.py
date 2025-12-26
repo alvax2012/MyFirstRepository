e5123fb56f8303bbaab2c5780e42546adff0ba6b
alvax2012 <39165799+alvax2012@users.noreply.github.com>
2025-12-26 18:42:30 +0800

    Update 43.py

--- 43.py
+++ 43.py
@@ -44,11 +44,18 @@ def choose_plural(amount: int, declensions: tuple):
 
 
 t0 = '%d.%m.%Y %H:%M'
+s0 = 'До выхода курса осталось:'
+s1 = 'Курс уже вышел!'
+d_tuple = ('день', 'дня', 'дней')
+h_tuple = ('час', 'часа', 'часов')
+m_tuple = ('минута', 'минуты', 'минут')
+
 # d0 = datetime.strptime('16.11.2021 06:30', t0)
 # d1 = datetime.strptime('08.11.2022 12:00', t0)
 
-d0 = datetime.strptime('17.11.2021 14:00', t0)
-d1 = datetime.strptime('17.11.2021 14:01', t0)
+d0 = datetime.strptime('08.11.2022 10:30', t0)
+d1 = datetime.strptime('08.11.2022 12:00', t0)
+
 
 d_cnt = (d1 - d0).days
 
@@ -58,24 +65,39 @@ h_cnt = s_all // 3600
 m_cnt = (s_all // 60) % 60
 s_cnt = s_all % 60
 
-if d_cnt < 0:
-    print(f'Курс уже вышел!')
-elif d_cnt and h_cnt and m_cnt:
-    print(
-        f'До выхода курса осталось: {choose_plural(d_cnt, ('дней', 'день'))} и {choose_plural(h_cnt, ('часов', 'час'))}')
-elif d_cnt and h_cnt == 0:
-    print(
-        f'До выхода курса осталось: {choose_plural(d_cnt, ('дней', 'день'))}')
+dc = {(0, 0, 0): 'Курс уже вышел!',
+      (0, 0, 1):  f'{s0} {choose_plural(m_cnt, m_tuple)}',
+      (0, 1, 0): f'{s0} {choose_plural(h_cnt, h_tuple)}',
+      (0, 1, 1): f'{s0} {choose_plural(h_cnt, h_tuple)} и {choose_plural(m_cnt, m_tuple)}',
+      (1, 0, 0): f'{s0} {choose_plural(d_cnt, d_tuple)}',
+      (1, 0, 1): f'{s0} {choose_plural(d_cnt, d_tuple)}',
+      (1, 1, 1): f'{s0} {choose_plural(d_cnt, d_tuple)} и {choose_plural(h_cnt, h_tuple)}',
+      }
 
-elif d_cnt == 0 and h_cnt and m_cnt:
-    print(
-        f'До выхода курса осталось: {choose_plural(h_cnt, ('часов', 'час'))} и {choose_plural(m_cnt, ('минут', 'минута'))}')
-elif d_cnt == 0 and h_cnt and m_cnt == 0:
-    print(
-        f'До выхода курса осталось: {choose_plural(d_cnt, ('часов', 'час'))}')
-elif h_cnt == 0:
-    print(
-        f'До выхода курса осталось: {choose_plural(m_cnt, ('минут', 'минута'))}')
+if d_cnt < 0:
+    print(s1)
+else:
+    print(dc[d_cnt != 0, h_cnt != 0, m_cnt != 0])
+
+
+# if d_cnt <= 0:
+#     print(f'Курс уже вышел!')
+# elif d_cnt and h_cnt and m_cnt:
+#     print(
+#         f'{s0} {choose_plural(d_cnt, ('день', 'дня', 'дней'))} и {choose_plural(h_cnt, ('час', 'часа', 'часов'))}')
+# elif d_cnt and h_cnt == 0:
+#     print(
+#         f'{s0} {choose_plural(d_cnt, ('день', 'дня', 'дней'))}')
+
+# elif d_cnt == 0 and h_cnt and m_cnt:
+#     print(
+#         f'{s0} {choose_plural(h_cnt, ('час', 'часа', 'часов'))} и {choose_plural(m_cnt, ('минута', 'минуты', 'минут'))}')
+# elif d_cnt == 0 and h_cnt and m_cnt == 0:
+#     print(
+#         f'{s0} {choose_plural(h_cnt, ('час', 'часа', 'часов'))}')
+# elif h_cnt == 0:
+#     print(
+#         f'{s0} {choose_plural(m_cnt, ('минута', 'минуты', 'минут'))}')
 
 # elif d_cnt == 0 and h_cnt != 0 and m_cnt != 0:
 #     print(f'До выхода курса осталось: {h_cnt} часов и {m_cnt} минут')
@@ -87,3 +109,7 @@ elif h_cnt == 0:
 
 print('d_cnt=', d_cnt, 'h_cnt=', h_cnt, 'm_cnt=',
       m_cnt, 's_cnt=', s_cnt, 's_all', s_all)
+
+print(choose_plural(20, ('час', 'часа', 'часов')))
+
+print(choose_plural(20, ('день', 'дня', 'дней')))
