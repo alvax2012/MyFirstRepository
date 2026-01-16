fe438be9ce94798d412bdc95c5e4648e16c5a7f5
alvax2012 <39165799+alvax2012@users.noreply.github.com>
2026-01-16 08:18:14 +0800

    Update 45.py

--- 45.py
+++ 45.py
@@ -1,4 +1,15 @@
 import calendar
+import datetime
 
 print(calendar.isleap(2024))
 print(calendar.isleap(2027))
+
+
+print(*[calendar.isleap(_)
+      for _ in (2007, 1993, 1994, 1992, 1991)], sep="\n")
+
+print(list(calendar.month_abbr).index('Dec'))
+
+d = datetime.datetime.strptime('2021-11-02', '%Y-%m-%d')
+# datetime.strptime(dt, '%Y%m%d')
+print(d.weekday())
