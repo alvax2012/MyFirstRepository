3d634d0f57cf3b889cc3cfdb426ede68d7a1ac28
alvax2012 <39165799+alvax2012@users.noreply.github.com>
2026-01-16 18:00:19 +0800

    Update 45.py

--- 45.py
+++ 45.py
@@ -1,5 +1,6 @@
 import calendar
-import datetime
+# import datetime
+from datetime import datetime, date
 
 print(calendar.isleap(2024))
 print(calendar.isleap(2027))
@@ -10,6 +11,18 @@ print(*[calendar.isleap(_)
 
 print(list(calendar.month_abbr).index('Dec'))
 
-d = datetime.datetime.strptime('2021-11-02', '%Y-%m-%d')
+d = datetime.strptime('2021-11-02', '%Y-%m-%d')
 # datetime.strptime(dt, '%Y%m%d')
 print(d.weekday())
+
+
+def get_days_in_month(y, m):
+    l = []
+    m_index = list(calendar.month_name).index(m)
+    for i in range(1, calendar.monthrange(y, m_index)[1]+1):
+        # l.append(date(y, m_index, i))
+        l.append(datetime(day=i, month=m_index, year=y).date())
+    return l
+
+
+print(get_days_in_month(2021, 'December'))
