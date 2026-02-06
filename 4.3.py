a3babb86c99e6c93ee41751bd0ba0ee4a8b7832e
alvax2012 <39165799+alvax2012@users.noreply.github.com>
2026-02-06 17:52:46 +0800

    1

--- 4.3.py
+++ 4.3.py
@@ -1,26 +1,27 @@
 
 import json
+import csv
 from pprint import pprint
+from datetime import datetime, time
+
+# countries = {'Monaco': 'Monaco', 'Iceland': 'Reykjavik', 'Kenya': 'Nairobi', 'Kazakhstan': 'Nur-Sultan',
+#              'Mali': 'Bamako', 'Colombia': 'Bogota', 'Finland': 'Helsinki', 'Costa Rica': 'San Jose',
+#              'Cuba': 'Havana', 'France': 'Paris', 'Gabon': 'Libreville', 'Liberia': 'Monrovia',
+#              'Angola': 'Luanda', 'India': 'New Delhi', 'Canada': 'Ottawa', 'Australia': 'Canberra'}
+
+# json_data = json.dumps(countries, indent=3, separators=(',', ' - '))
 
 
-countries = {'Monaco': 'Monaco', 'Iceland': 'Reykjavik', 'Kenya': 'Nairobi', 'Kazakhstan': 'Nur-Sultan',
-             'Mali': 'Bamako', 'Colombia': 'Bogota', 'Finland': 'Helsinki', 'Costa Rica': 'San Jose',
-             'Cuba': 'Havana', 'France': 'Paris', 'Gabon': 'Libreville', 'Liberia': 'Monrovia',
-             'Angola': 'Luanda', 'India': 'New Delhi', 'Canada': 'Ottawa', 'Australia': 'Canberra'}
+# club1 = {"name": "FC Byern Munchen", "country": "Germany", "founded": 1900,
+#          "trainer": "Julian Nagelsmann", "goalkeeper": "M. Neuer", "league_position": 1}
 
-json_data = json.dumps(countries, indent=3, separators=(',', ' - '))
+# club2 = {"name": "FC Barcelona", "country": "Spain", "founded": 1899,
+#          "trainer": "Xavier Creus", "goalkeeper": "M. Ter Stegen", "league_position": 7}
 
+# club3 = {"name": "FC Manchester United", "country": "England", "founded": 1878,
+#          "trainer": "Michael Carrick", "goalkeeper": "D. De Gea", "league_position": 8}
 
-club1 = {"name": "FC Byern Munchen", "country": "Germany", "founded": 1900,
-         "trainer": "Julian Nagelsmann", "goalkeeper": "M. Neuer", "league_position": 1}
-
-club2 = {"name": "FC Barcelona", "country": "Spain", "founded": 1899,
-         "trainer": "Xavier Creus", "goalkeeper": "M. Ter Stegen", "league_position": 7}
-
-club3 = {"name": "FC Manchester United", "country": "England", "founded": 1878,
-         "trainer": "Michael Carrick", "goalkeeper": "D. De Gea", "league_position": 8}
-
-data = [club1, club2, club3]
+# data = [club1, club2, club3]
 
 # with open('data.json', 'w') as file:
 #     json.dump(data, file, indent=3)
@@ -55,30 +56,118 @@ data = [club1, club2, club3]
 
 
 def istype(i):
-    if isinstance(i, str):
+    if isinstance(i, bool):
+        return not i
+    elif isinstance(i, str):
         return i+'!'
     elif isinstance(i, int):
         return i+1
-    elif isinstance(i, bool):
-        return not i
     elif isinstance(i, list):
-        return not i*2
+        return i*2
     elif isinstance(i, dict):
         i.update({'newkey': None})
         return i
-    elif i == 'null':
-        return None
     else:
-        return i
+        pass
 
 
-with open('data.json') as file:
-    data = json.load(file)
+# with open('data.json', encoding='utf-8') as file:
+#     data = json.load(file)
+
+# # l1 = [(1, 2), 1, 'ww', [1, 2], {1: 3}, False, None]
+# # print([istype(i) for i in l1 if i != None])
+
+# print(type(data))
+# data = [istype(i) for i in data if i != None]
+# with open('updated_data.json', 'w', encoding='utf-8') as file:
+#     # json.dump([1, 3], file)
+#     json.dump(data, file, indent=3)
+
+# res = []
+# d = {}
+# with open('people.json', encoding='utf-8') as file:
+#     data = json.load(file)
+
 
 # for i in data:
-#     print(i)
+#     d.update(i)
 
-print(data)
-with open('updated_data.json', 'w') as file:
-    json.dump([1, 'ee'], file)
-    # json.dump(list(map(istype, data)), file, indent=3)
+# print(d.keys())
+
+# [i.update({k: None}) for i in data for k in d if k not in i]
+
+
+# #print(data)
+# with open('updated_people.json', 'w', encoding='utf-8') as file:
+#     json.dump(data, file, indent=3)
+
+l = [1, 2]
+for i in l:
+    i = 111
+print(l)
+
+
+# d = {}
+# with open('countries.json', encoding='utf-8') as file:
+#     data = json.load(file)
+
+# for i in data:
+#     v, k = i.values()
+#     d.setdefault(k, []).append(v)
+
+# print(d)
+# with open('religion.json', 'w', encoding='utf-8') as file:
+#     json.dump(d, file, indent=3)
+
+# d = {}
+# with open('playgrounds.csv', encoding='utf-8') as in_file:
+#     reader = csv.DictReader(in_file, delimiter=';')
+#     col = reader.fieldnames
+#     rows = list(reader)
+
+# # print(col)
+# # print(rows)
+
+
+# for i in rows:
+#     d.setdefault(i[col[1]], {}).setdefault(i[col[2]], []).append(i[col[3]])
+
+# print(d)
+# with open('addresses.json', 'w', encoding='utf-8') as file:
+#     json.dump(d, file, indent=3, ensure_ascii=0)
+
+
+# d = {}
+# with open('students.json', encoding='utf-8') as file:
+#     data = json.load(file)
+# col = ['name', 'phone']
+
+
+# res = [{'name': i['name'], 'phone': i['phone']}
+#        for i in data if i['age'] >= 18 and i['progress'] >= 75]
+# res.sort(key=lambda x: x['name'])
+# print(res)
+# with open('data.csv', 'w', encoding='utf-8', newline='') as out_file:
+#     writer = csv.DictWriter(out_file, fieldnames=col)
+#     writer.writeheader()
+#     writer.writerows(res)
+
+
+# d = {}
+l = []
+with open('pools.json', encoding='utf-8') as file:
+    data = json.load(file)
+
+for i in data:
+    adr = i['Address']
+    work = i['WorkingHoursSummer']['Понедельник'].split('-')
+    # datetime.strptime(work[0], '%H:%M')
+    d1 = time(hour=int(work[0].split(':')[0]))
+    d2 = time(hour=int(work[1].split(':')[0]))
+    dim = i['DimensionsSummer']
+    if d1.hour <= 10 and d2.hour >= 12:
+        l.append((adr,   dim['Length'], dim['Width']))
+
+l.sort(key=lambda x: (-x[1], -x[2]))
+res = l[0]
+print(f'{res[1]}x{res[2]}', res[0], sep='\n')
