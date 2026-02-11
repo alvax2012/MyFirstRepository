2061955357f2746ffdaae5ffa5bf66bc4c9a71a2
alvax2012 <39165799+alvax2012@users.noreply.github.com>
2026-02-11 18:31:53 +0800

    1

--- 4.5.py
+++ 4.5.py
@@ -97,5 +97,78 @@ with ZipFile('desktop.zip') as zip_file:
     info = zip_file.infolist()
 
 
+d = {}
+d1 = {}
 for i in info:
-    print(i.filename)
+    l = i.filename.split('/')
+
+    if len(l) <= 2:
+        d[l[0]] = None
+    elif l[-1]:
+        d[l[-1]] = l[-2]
+    else:
+        d[l[-2]] = l[-3]
+
+    d1[i.filename.split('/')[-1]] = i.file_size
+    print(i.filename, l)
+
+print(d)
+
+
+def path_rec(i, di):
+    res = '/'
+    if di:
+        res += f'{di}/{i}'
+    else:
+        res += f'{i}'
+    return res
+
+
+print()
+l = []
+for i in d:
+    s = {}
+    if d[i]:
+        di = d[i]
+        ii = i
+        # s = {}
+        while di:
+            s.setdefault(i, []).append(di)
+            ii = di
+            di = d[ii]
+        print(s)
+    else:
+        s.setdefault(i)
+        print(s)
+    l.append(s)
+print()
+for i in l:
+    k, v = list(i.items())[0]
+    if v:
+        # print(f'/{'/'.join(v[::-1])}/{k}')
+        print(f'{'  '*len(v)}{k}', d1[k])
+    else:
+        # print(f'/{k}')
+        print(f'{k}')
+
+
+print('-'*30)
+print(d1)
+
+# d1 = [
+#     {'fun': [{'movies': None}, {
+#         'songs': ['Alexandra Savior Crying All the Time.mp3']}]},
+#     {'games': [{'not released': 'Hollow Knight Silksong.exe'},
+#                'Psychonauts 2.exe']},
+#     {'images': ['code.jpeg', 'stepik.png']}
+
+# ]
+
+# d2 = {'fun': None, 'movies': 'fun', 'songs': 'fun',
+#       'Alexandra Savior Crying All the Time.mp3': 'songs'}
+
+# for k, v in d2.items():
+#     if not v:
+#         print(k)
+#     else:
+#         print(f'{v}/{k}')
