f37f02cc3533fd4094f9428c2615fb29f173a7d9
alvax2012 <39165799+alvax2012@users.noreply.github.com>
2026-03-02 18:14:46 +0800

    Update test1.py

--- test1.py
+++ test1.py
@@ -29,16 +29,113 @@ for i in l:
 print('=', *l_out)
 
 
-n = 7
+# n = 7
 
-k = 0
-for i in range(1, n+1):
-    if k == n:
-        print('==')
-        break
-    for j in range(i):
-        print(i, end=' ')
-        k += 1
-    # print('k', k)
+# k = 0
+# for i in range(1, n+1):
 
-print(n)
+#     for j in range(i):
+
+#         k += 1
+#         if k == n+1:
+#             break
+#         print(i, end=' ')
+#     if k == n+1:
+#         break
+#     # print('k', k)
+
+# # print(n)
+
+
+l = [8, 8, 2, 7, 8, 8, 2, 4]
+x = 8
+
+
+if x in l:
+    m = -1
+else:
+    print('Отсутствует')
+    exit()
+
+for i in l:
+    if i == x:
+
+        try:
+            m = l.index(i, m+1)
+            print(m, end=' ')
+        except ValueError:
+            break
+
+
+for i in enumerate(l):
+    print(i)
+
+l = [1, 2, 3, 4, 5, 6]
+l1 = l.copy()
+ld = []
+di = 0
+for i in range(len(l1)):
+    if l1[i] % 2:
+        del l[i-di]
+        # l[i] = int(x/2)
+        di += 1
+        # print(i-di, di)
+    else:
+        l[i-di] = int(l1[i] / 2)
+    # i -= di
+#     else:
+#         ld.append(i)
+# for i in ld:
+#     del l[i]
+print(l)
+
+
+def update_dictionary(d, key, value):
+    if key in d:
+        # d[key] += [value]
+        d[key].append(value)
+    else:
+        d.setdefault(key*2, []).append(value)
+        # d[key*2] = []
+
+
+d = {}
+print(update_dictionary(d, 1, -1))  # None
+print(d)                            # {2: [-1]}
+update_dictionary(d, 2, -2)
+print(d)                            # {2: [-1, -2]}
+update_dictionary(d, 1, -3)
+print(d)
+
+s = '''Спартак;9;Зенит;10
+Локомотив;12;Зенит;3
+Спартак;8;Локомотив;15'''
+d = {}
+l = [['Спартак', '9', 'Зенит', '10'], ['Локомотив', '12',
+                                       'Зенит', '3'], ['Спартак', '8', 'Локомотив', '15']]
+s1, s2 = 0, 0
+for i in l:
+    k1, c1, k2, c2 = i
+
+    if int(c1) < int(c2):
+        s1 = 0
+        s2 = 3
+    elif int(c1) > int(c2):
+        s1 = 3
+        s2 = 0
+    else:
+        s1 = 1
+        s2 = 1
+
+    d.setdefault(k1, []).append(s1)
+    d.setdefault(k2, []).append(s2)
+    print(i, k1, c1, k2, c2, s1, s2)
+
+for k, v in d.items():
+    p0 = len(list(filter(lambda x: x == 0, v)))
+    p1 = len(list(filter(lambda x: x == 1, v)))
+    p3 = len(list(filter(lambda x: x == 3, v)))
+
+    print(f'{k}:{len(v)} {p3} {p1} {p0} {sum(v)}', v)
+
+print(d)
