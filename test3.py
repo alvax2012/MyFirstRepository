3d166dd8a43200242b3e9f10298cbb078b28c04e
alvax2012 <39165799+alvax2012@users.noreply.github.com>
2026-03-25 07:33:43 +0800

    1

--- test3.py
+++ test3.py
@@ -1,3 +1,7 @@
+from collections import defaultdict
+import pandas as pd
+
+
 def null_dec(func):
     y = 123
     return func(y)
@@ -11,3 +15,20 @@ def greet(y):
 greet1 = null_dec(greet)
 
 print(greet1)
+
+
+sample = [1, 2, 3, 4, 4, 4, 5, 6, 7, 8, 9]
+
+sr = pd.Series(sample)
+
+print(sr.median())
+
+
+my_dict = defaultdict(list)
+
+for i in sample:
+    # if i == 4:
+    my_dict[i].append(i)
+
+
+print(my_dict)
