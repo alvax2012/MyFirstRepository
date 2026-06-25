ff0f4ed3353377fb3530077bb0c262a0c075f6d5
alvax2012 <39165799+alvax2012@users.noreply.github.com>
2026-06-25 18:03:27 +0800

    1

--- 7.3.py
+++ 7.3.py
@@ -50,3 +50,19 @@ print(t1)
 
 numbers = list(filter(int, ['1', '2', '3', '4', '5']))
 print(numbers)
+
+
+s = '111s'
+
+months_dict = {1: 'January', 2: 'February', 3: 'March', 4: 'April', 5: 'May', 6: 'June',
+               7: 'July', 8: 'August', 9: 'September', 10: 'October', 11: 'November', 12: 'December'}
+
+
+try:
+    s_out = months_dict[int(s)]
+except ValueError:
+    s_out = 'Введено некорректное значение'
+except KeyError:
+    s_out = 'Введено число из недопустимого диапазона'
+
+print(s_out)
