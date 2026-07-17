b2aed7a0f75511bb82313b93867b106972e318c8
alvax2012 <39165799+alvax2012@users.noreply.github.com>
2026-07-17 18:19:13 +0800

    Update 8.py

--- 8.py
+++ 8.py
@@ -6,3 +6,43 @@ def bee(n):
 
 
 bee(2)
+
+
+def triangle(n):
+    i = 1
+
+    def prn(i):
+        if i <= n:
+            print('*'*i)
+            prn(i+1)
+    prn(i)
+
+
+# triangle(3)
+
+def triangle(n):
+    if n > 0:
+        triangle(n-1)
+    print('*'*n)
+
+
+def triangle(h):
+    if h > 1:
+        triangle(h - 1)
+    print('*' * h)
+
+
+print()
+triangle(5)
+
+print('--')
+
+
+def wtch(n):
+    if n > 0:
+        print('*'*n)
+        wtch(n-1)
+        print('*'*n)
+
+
+wtch(4)
