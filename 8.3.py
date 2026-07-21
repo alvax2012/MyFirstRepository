66ee45c546c2dadcb2287ee09005844ccca8741f
alvax2012 <39165799+alvax2012@users.noreply.github.com>
2026-07-21 18:15:42 +0800

    1

--- 8.3.py
+++ 8.3.py
@@ -35,3 +35,42 @@ def ndig(n):
 
 
 print(ndig(n))
+
+
+def number_of_frogs(y):
+    if y == 1:
+        return 77
+    return 3*(number_of_frogs(y-1)-30)
+
+
+print(number_of_frogs(2))
+
+
+def range_sum(num, start, end):
+    if start == end:
+        return num[end]
+    return num[end] + range_sum(num, start, end - 1)
+
+
+print(range_sum([1, 2, 3, 4, 5, 6, 7, 8, 9], 3, 7))
+
+
+def get_pow(a, n):
+    if not n:
+        return 1
+    return a*get_pow(a, n-1)
+
+
+print(get_pow(5, 2))
+
+
+def get_fast_pow(a, n):
+    if n == 1:
+        return a
+    elif n % 2:
+        return (a**2)*(n/2)
+    else:
+        return a*(a**(n-1))
+
+
+print(get_fast_pow(5, 2))
