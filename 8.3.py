ac2a18ede3ae8707f77fc04ebe2acbdddde8b365
alvax2012 <39165799+alvax2012@users.noreply.github.com>
2026-07-24 21:25:55 +0800

    Update 8.3.py

--- 8.3.py
+++ 8.3.py
@@ -65,12 +65,12 @@ print(get_pow(5, 2))
 
 
 def get_fast_pow(a, n):
-    if n == 1:
-        return a
+    if n == 0:
+        return 1
     elif n % 2:
         return a*(get_fast_pow(a, n-1))
     else:
-        return get_fast_pow(a, n-1)**2
+        return get_fast_pow(a*a, n/2)
 
 
 print()
