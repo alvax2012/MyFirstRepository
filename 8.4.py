f251efd827ad4da3403bd3d5cd5c222e5f3d2efc
alvax2012 <39165799+alvax2012@users.noreply.github.com>
2026-07-29 17:02:20 +0800

    Create 8.4.py

---
+++ 8.4.py
@@ -0,0 +1,35 @@
+def find_key(data, key):
+    if key in data:
+        return data[key]                # базовый случай
+
+    for v in data.values():
+        if type(v) == dict:
+            value = find_key(v, key)    # рекурсивный случай
+            if value is not None:
+                return value
+
+
+info = {'name': 'Alyson',
+        'birthday': {'day': 24, 'month': 'March', 'year': 1974},
+        'family':   {'mother': 'Emilie Posner', 'father': 'Alan Hannigan'}}
+
+print(find_key(info, 'year'))
+print(find_key(info, 'father'))
+
+
+def recursive_sum(l):
+
+    def sm(s, n):
+        if type(n) == int:
+            return s + n
+
+        for i in l:
+            sm(s, i)
+
+    return sm(0, l)
+
+
+my_list = [1,  2]
+# my_list = [1, [4, 4], 2]
+
+print(recursive_sum(my_list))
