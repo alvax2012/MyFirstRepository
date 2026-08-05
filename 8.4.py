198cbd11b724bc9fa53b50f649e184f156890fba
alvax2012 <39165799+alvax2012@users.noreply.github.com>
2026-08-05 21:12:55 +0800

    Update 8.4.py

--- 8.4.py
+++ 8.4.py
@@ -28,86 +28,17 @@ def recursive_sum(n):
     return s
 
 
-my_list = [1, 2, 5]
-# my_list = [1, [4, 4], 2]
+# my_list = [1, 2, 5]
+my_list = [1, [4, 4], 2]
 
 print('==', recursive_sum(my_list))
 
 print()
 
-# l_out = []
+
+def linear(my_list):
 
 
-def linear(n):
-    l_out = []
+my_list = [3, [4], [5, [6, [7, 8]]]]
 
-    def lsn(n):
-        if isinstance(n, int):
-            return n
-
-        for i in n:
-            k = lsn(i)
-            if k:
-                l_out.append(k)
-    lsn(n)
-    return l_out
-
-
-def linear(li):
-    res = []
-    print(id(res))
-    for elem in li:
-        if isinstance(elem, list):
-            res.extend(linear(elem))
-        else:
-            res.append(elem)
-    return res
-
-
-def linear(data):
-    total = []
-
-    def loc(data):
-        if type(data) == int:
-            total.append(data)
-            return
-        if type(data) == list:
-            for i in data:
-                loc(i)              # рекурсивный случай
-    loc(data)
-    return total
-
-
-# my_list = [[3, 2, 5345, 65, 7, 777, 0, 43, 65, 754, 3, 1, 2]]
-my_list = [3, 1, [4, 2]]
-# my_list = [3, [4], [5, [6, [7, 8]]]]
-
-print('=', linear(my_list))
-
-
-# def tt1(l):
-#     return l
-
-# l = [1, 2]
-# print(id(l), id(tt1(l)))
-
-print()
-
-
-def get_value(d, key):
-    res = ''
-    if key in d:
-        res = d[key]
-        return res
-
-    for k in d:
-        if isinstance(k, dict):
-            get_value(k, key)
-
-    return res
-
-
-data = {'firstName': 'Тимур', 'lastName': 'Гуев', 'birthDate': {'day': 10, 'month': 'October', 'year': 1993}, 'address': {
-    'streetAddress': 'Часовая 25, кв. 127', 'city': {'region': 'Московская область', 'type': 'город', 'cityName': 'Москва'}, 'postalCode': '125315'}}
-
-print('res=', get_value(data, 'cityName'))
+print(linear(my_list))
