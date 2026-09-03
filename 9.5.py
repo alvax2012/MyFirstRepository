0b46a0204ccf1fda010da3f309a76c728e7518c7
alvax2012 <39165799+alvax2012@users.noreply.github.com>
2026-09-04 07:53:29 +0800

    Update 9.5.py

--- 9.5.py
+++ 9.5.py
@@ -163,7 +163,7 @@ def sort_priority(numbers, group):
 
 numbers = [8, 3, 1, 2, 5, 4, 7, 6]
 group = {5, 7, 2, 3}
-sort_priority(numbers, group)
+print('=', sort_priority(numbers, group))
 
 print(numbers)
 
