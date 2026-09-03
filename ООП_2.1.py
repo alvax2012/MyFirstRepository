250881a4132fbd0c29cf4d89fd74a86bd0224b33
alvax2012 <39165799+alvax2012@users.noreply.github.com>
2026-09-03 08:09:28 +0800

    Update ООП_2.1.py

--- ООП_2.1.py
+++ ООП_2.1.py
@@ -27,3 +27,42 @@ def is_integer(n):
 
 
 print(is_integer('-199'))
+
+
+def is_decimal(n):
+    try:
+        float(n)
+        return True
+    except ValueError:
+        return False
+
+
+print(is_decimal('.-2'))
+
+
+def is_fraction(x):
+    def is_integer(n):
+        try:
+            int(n)
+            return True
+        except ValueError:
+            return False
+
+    res = x.split('/')
+    print(res)
+    if len(res) != 2:
+        return False
+    elif is_integer(res[0]) and is_integer(res[1]) and int(res[1]) > 0:
+        return True
+    else:
+        return False
+
+
+print('--', is_fraction('1/-2'))
+
+
+def intersperse(iterable, delimiter):
+    return delimiter.join(iterable)
+
+
+print(*intersperse([1, 2, 3], '-'))
