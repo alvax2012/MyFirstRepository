f1a98e14b5f8390c51242af45c33a531b34afc5d
alvax2012 <39165799+alvax2012@users.noreply.github.com>
2026-09-03 18:36:59 +0800

    Update 9.5.py

--- 9.5.py
+++ 9.5.py
@@ -1,3 +1,9 @@
+from datetime import datetime
+from datetime import datetime, UTC
+from datetime import date
+import time
+
+
 def f(a):
     def g():
         nonlocal a
@@ -86,3 +92,79 @@ def outer(x):
 
 
 print(outer(10)())
+
+
+def date_formatter(country_code):
+    df = {'ru': '%d.%m.%y',
+          'us': 'MM-DD-YYYY',
+          'ca': 'YYYY-MM-DD',
+          'br': 'DD/MM/YYYY',
+          'fr': 'DD.MM.YYYY',
+          'pt': 'DD-MM-YYYY'}
+    t = df[country_code]
+    return lambda d: d.strftime(df[country_code])
+
+
+date_ru = date_formatter('ru')
+today = date(2022, 1, 25)
+print(date_ru(today), today.strftime('%d.%m.%y'))
+
+
+text = 'Experiment Date 01/28/2005; Time 23:50:13'
+
+dt = datetime.strptime(text, 'Experiment Date %m/%d/%Y; Time %H:%M:%S')
+
+print('dt', dt, dt.strftime('%m'))
+
+
+def factorial_recurrent(n):                  # рекурсивная функция
+    if n == 0:
+        return 1
+    return n * factorial_recurrent(n - 1)
+
+
+def factorial_classic(n):                    # итеративная функция
+    f = 1
+    for i in range(2, n + 1):
+        f *= i
+    return f
+
+
+def get_the_fastest_func(*args, arg):
+
+    d = {}
+    for f in args:
+        start_time = time.monotonic()
+        f(arg)
+        end_time = time.monotonic()
+        d[f] = (end_time - start_time)
+
+    return d
+
+
+print(get_the_fastest_func(factorial_recurrent, factorial_classic, arg=100))
+
+
+def sort_priority(num, gr):
+    l = []
+    for i in gr:
+        if i in num:
+            num.remove(i)
+            l.append(i)
+    num.sort()
+    l.sort()
+    num[0:0] = l
+    return num
+
+
+def sort_priority(numbers, group):
+    numbers.sort(key=lambda x: (x not in group, x))
+
+
+numbers = [8, 3, 1, 2, 5, 4, 7, 6]
+group = {5, 7, 2, 3}
+sort_priority(numbers, group)
+
+print(numbers)
+
+# [2, 3, 5, 7, 1, 4, 6, 8]
