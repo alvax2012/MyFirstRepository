852d15bdaa5c0a5886b7515b6baba43aaffbf2c2
alvax2012 <39165799+alvax2012@users.noreply.github.com>
2026-09-08 07:56:21 +0800

    Update 9.6.py

--- 9.6.py
+++ 9.6.py
@@ -1,7 +1,3 @@
-def top_grade(grades: dict[str, str | list[int]]) -> dict[str, str | int]:
-    return {'name': grades['name'],  'top_grade': max(grades['grades'])}
-
-
 print(__name__)
 
 
@@ -12,13 +8,3 @@ def top_grade(grades):
 info = {'name': 'Timur', 'grades': [30, 57, 99]}
 
 print(top_grade(info))
-
-
-def cyclic_shift(numbers: list[int | float], step: int) -> None:
-    for i in range(step):
-
-
-numbers = [1, 2, 3, 4, 5]
-cyclic_shift(numbers, 1)
-
-print(numbers)
